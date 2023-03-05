05299a95b450ba0c8a11933eb003b4e6fbbeef2e
Bilyk Mykola <sadovast7@gmail.com>
2023-03-05 21:51:07 +0200

    first commit test arquillian

--- src/main/java/com/fastfood/fastfood/dao/MenuDAO.java
+++ src/main/java/com/fastfood/fastfood/dao/MenuDAO.java
@@ -2,7 +2,6 @@ package com.fastfood.fastfood.dao;
 
 import java.io.Serializable;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
